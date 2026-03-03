7829a3ddc0c65586cc47cee127fb902385c6d9b0
Temin2972 <temin2972@gmail.com>
2026-03-03 14:27:54 +0700

    fixes

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -11,7 +11,6 @@ export { default as Avatar } from './Avatar'
 export { default as Card } from './Card'
 export { default as Modal } from './Modal'
 export { default as Badge } from './Badge'
-export { default as CalmModeToggle } from './CalmModeToggle'
 export { default as AnonymousAvatar } from './AnonymousAvatar'
 
 
